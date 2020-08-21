30422631d02cbd0e5bcc3db7fa6c2e7f50213078
duchuynh0899 <54980175+duchuynh0899@users.noreply.github.com>
2020-08-21 20:57:46 +0700

    up

--- js/slide.js
+++ js/slide.js
@@ -29,3 +29,59 @@ window.addEventListener("load", function(event) {
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+
+$(".product-colors5 span").click(function(){
+  $(".product-colors5 span").removeClass("active");
+  $(this).addClass("active");
+  $(".product-pic5").css("background-image",$(this).attr("data-pic"));
+  
+   $(".main__product--price5 p").css("color",$(this).attr("data-color"));
+  
+});
+
+$(".product-colors4 span").click(function(){
+  $(".product-colors4 span").removeClass("active");
+  $(this).addClass("active");
+  $(".product-pic4").css("background-image",$(this).attr("data-pic"));
+  
+   $(".main__product--price4 p").css("color",$(this).attr("data-color"));
+  
+});
+
+$(".product-colors3 span").click(function(){
+  $(".product-colors3 span").removeClass("active");
+  $(this).addClass("active");
+  $(".product-pic3").css("background-image",$(this).attr("data-pic"));
+  
+   $(".main__product--price3 p").css("color",$(this).attr("data-color"));
+  
+});
+
+
+$(".product-colors2 span").click(function(){
+  $(".product-colors2 span").removeClass("active");
+  $(this).addClass("active");
+  $(".product-pic2").css("background-image",$(this).attr("data-pic"));
+  
+   $(".main__product--price2 p").css("color",$(this).attr("data-color"));
+  
+});
+
+$(".product-colors1 span").click(function(){
+  $(".product-colors1 span").removeClass("active");
+  $(this).addClass("active");
+  $(".product-pic1").css("background-image",$(this).attr("data-pic"));
+  
+   $(".main__product--price1 p").css("color",$(this).attr("data-color"));
+  
+});
+
+$(".product-colors0 span").click(function(){
+  $(".product-colors0 span").removeClass("active");
+  $(this).addClass("active");
+  $(".product-pic0").css("background-image",$(this).attr("data-pic"));
+  
+   $(".main__product--price0 p").css("color",$(this).attr("data-color"));
+  
+});
