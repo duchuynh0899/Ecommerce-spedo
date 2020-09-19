317d0c393d9ede53a0cbcc820c6965b05de5e56e
duchuynh0899 <54980175+duchuynh0899@users.noreply.github.com>
2020-09-19 15:54:03 +0700

    up

--- js/slide.js
+++ js/slide.js
@@ -95,3 +95,20 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+
+// menu
+
+
+
+const menu = document.querySelector(".menu");
+const header = document.querySelector(".header1");
+const navbar = document.querySelector(".navmenu__li");
+const navbar_ul = document.querySelector(".navmenu__li> ul");
+menu.addEventListener("click", () => {  
+   menu.classList.toggle("active");
+   header.classList.toggle("active");
+   navbar.classList.toggle('active');
+   navbar_ul.classList.toggle("active")
+   
+});
